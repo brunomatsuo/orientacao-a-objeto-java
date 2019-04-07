c39b0554480e2eec82118cec8e1a3fb09cdef6d2
brunomatsuo <brunocomat@gmail.com>
2019-04-07 20:03:28 -0300

    Adicionando projeto

---
+++ src/TotalValores.java
@@ -0,0 +1,13 @@
+
+public class TotalValores {
+	
+	public static void main(String[] args) {
+		Conta conta = new Conta(1337, 24226);
+		
+		System.out.println(conta.getAgencia());
+		
+			
+		System.out.println(Conta.getTotal());
+	}
+
+}
